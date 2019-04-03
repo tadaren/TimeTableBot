8a19f60763773adeb56ba626eedc2a2ee57fcd00
tadaren <ptymaato@yahoo.co.jp>
2019-04-03 23:47:45 +0900

    [add] eventmanagerを追加

---
+++ core/usecase/eventmanager.py
@@ -0,0 +1,22 @@
+def get_by_id(id):
+    pass
+
+
+def get_by_day(first_day, last_day):
+    pass
+
+
+def get_by_tag(tag):
+    pass
+
+
+def update(id, name, date, tag):
+    pass
+
+
+def delete(id):
+    pass
+
+
+def add(subject, deteil, deadline, tag):
+    pass
