6c77f1ba211f6165f0f9447b0b07040065714416
tadaren <ptymaato@yahoo.co.jp>
2019-04-04 22:21:35 +0900

    [add] taskmanagerを追加

---
+++ core/usecase/taskmanager.py
@@ -0,0 +1,26 @@
+def get_by_id(id):
+    pass
+
+
+def get_by_day(firstDay, lastDay):
+    pass
+
+
+def get_by_subject(subject):
+    pass
+
+
+def get_by_tag(tag):
+    pass
+
+
+def update(id, subject, detail, deadline, tag):
+    pass
+
+
+def delete(id):
+    pass
+
+
+def add(subject, detail, deadline, tag):
+    pass
