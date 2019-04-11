cad67e53a4f35dbe228bf9119bc24087ed5c8c2f
tadaren <ptymaato@yahoo.co.jp>
2019-04-11 21:48:11 +0900

    [add] TimeTableManagerを追加

---
+++ core/usecase/timetablemanager.py
@@ -0,0 +1,18 @@
+import datetime
+from typing import List
+
+from domain.timetablechange import TimeTableChange
+
+
+class TimeTableManager:
+    def __init__(self, timetable_repository, timetable_change_repository):
+        self.tt_repository = timetable_repository
+        self.ttc_repository = timetable_change_repository
+
+    def get(self, date: datetime.date):
+        timetable = self.tt_repository.find({'date': date.isoformat()})
+        timetable_change = self.ttc_repository.find({'date': date.isoformat()})
+        timetable.change = timetable_change
+
+    def add(self, date: datetime.date, period: int, subject: str, tags: List[str]):
+        self.ttc_repository.insert(TimeTableChange('', date, period, subject, tags))
