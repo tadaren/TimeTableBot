8afbd589d1de7839ce263d7747e4dd1e23438804
tadaren <ptymaato@yahoo.co.jp>
2019-04-12 23:02:07 +0900

    [add] TimeTableRepository を追加

---
+++ core/repository/timetablerepository.py
@@ -0,0 +1,16 @@
+import datetime
+
+from pymongo.collection import Collection
+
+from domain.timetable import TimeTable
+
+
+class TimeTableRepository:
+    def __init__(self, db, collection_name):
+        self._db = db
+        self._collection_name = collection_name
+
+    def find(self, key):
+        tt_collection = self._db[self._collection_name]  # type: Collection
+        res = tt_collection.find(key)
+        return [TimeTable(datetime.date.fromisoformat(e['date']), e['subjects'], []) for e in res]
