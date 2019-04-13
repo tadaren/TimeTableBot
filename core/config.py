2864029e6b4df53158b7ba3bce3d1d398653a7f9
tadaren <ptymaato@yahoo.co.jp>
2019-04-13 09:55:51 +0900

    [add] config.pyにコレクションの名前定数を追加
    課題用: TASK_COLLECTION_NAME
    時間割用: TIMETABLE_COLLECTION_NAME
    時間割変更用: TIMETABLE_CHANGE_COLLECTION_NAME

--- core/config.py
+++ core/config.py
@@ -2,3 +2,6 @@ import os
 
 DB_URL = os.environ['DB_URL']
 EVENT_COLLECTION_NAME = 'event'
+TASK_COLLECTION_NAME = 'task'
+TIMETABLE_COLLECTION_NAME = 'timetable'
+TIMETABLE_CHANGE_COLLECTION_NAME = 'timetable_change'
