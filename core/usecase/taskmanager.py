e55ea5883126a69818f14f049cdf53b41b3bb922
tadaren <ptymaato@yahoo.co.jp>
2019-04-14 19:51:11 +0900

    [add] TaskManagerを追加

--- core/usecase/taskmanager.py
+++ core/usecase/taskmanager.py
@@ -1,26 +1,30 @@
-def get_by_id(id):
-    pass
+import datetime
+from typing import List
+import bson
+
+from domain.task import Task
+from util import build_date_query
 
 
-def get_by_day(firstDay, lastDay):
-    pass
+class TaskManager:
+    def __init__(self, repository):
+        self.repository = repository
 
+    def get(self, first_day: datetime.date = None, last_day: datetime.date = None, tags: List[str] = []):
+        query = {}
+        date_query = build_date_query(first_day, last_day)
+        if date_query:
+            query['date'] = date_query
+        if tags:
+            query['tag'] = {'$all': tags}
+        ret = self.repository.find(query)
+        return ret
 
-def get_by_subject(subject):
-    pass
+    def update(self, task_id, subject, detail, deadline, tag):
+        self.repository.update({'_id': bson.ObjectId(task_id)}, Task(task_id, subject, detail, deadline, tag))
 
+    def delete(self, task_id):
+        self.repository.delete({'_id': bson.ObjectId(task_id)})
 
-def get_by_tag(tag):
-    pass
-
-
-def update(id, subject, detail, deadline, tag):
-    pass
-
-
-def delete(id):
-    pass
-
-
-def add(subject, detail, deadline, tag):
-    pass
+    def add(self, subject, detail, deadline, tag):
+        self.repository.insert(Task('', subject, detail, deadline, tag))
